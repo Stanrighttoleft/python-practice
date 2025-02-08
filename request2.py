6cdd2ed8e880ad4e8e2da42a8bbd361bf975ccfd
stan <moon_saga@yahoo.com.tw>
2025-02-08 22:32:24 +0800

    tools in bs4

--- request2.py
+++ request2.py
@@ -9,3 +9,26 @@ bsp=BeautifulSoup(html.text,'html.parser')
 
 datatitle=bsp.select("title")
 print(datatitle)
+
+dataid=bsp.select("#cmsg")
+print(dataid)
+
+datainfo=bsp.select("html head meta")
+print(datainfo)
+
+datameta=bsp.find('meta')
+print(datameta)
+
+datametaall=bsp.find_all('meta')
+print(datametaall)
+
+datalink=bsp.find('a')
+print(datalink)
+
+datalinka=bsp.find_all('a')
+print(datalinka)
+
+datafooter=bsp.select("#footer")
+print(datafooter)
+
+#tag can use find, select is used for id
