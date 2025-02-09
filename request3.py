8bfe8109606d95a9c2cba955ea739a041e969386
stan <moon_saga@yahoo.com.tw>
2025-02-09 16:46:01 +0800

    webpage element get in request3.py

--- request3.py
+++ request3.py
@@ -14,7 +14,24 @@ data_div=bsp.find('div',{"id":"fb-root"})
 #print(data_div)
 
 data_c=bsp.find_all('div',{"class":"btn"})
-print(data_c)
+#print(data_c)
 
 for info in data_c:
     print(info.text)
+
+data_info=bsp.find_all(['link','img'])
+
+#print(data_info)
+for addr in data_info:
+    print(addr)
+    imgsrc=addr.get("src")
+    print(imgsrc)
+
+    if imgsrc !=None and imgsrc.startswith("https"):
+        print(imgsrc)
+
+    alink=addr.get("href")
+    #print(alink)
+
+    if alink !=None and alink.startswith("https"):
+        print(alink)
